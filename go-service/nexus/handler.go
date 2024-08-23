0a4ee35a612774b62062e0d2420464d49e7e86a8
Adrian Wang <wyz17601402786@gmail.com>
2024-08-23 17:43:57 +0800

    FEAT: A demo about how to use thrift streaming

--- go-service/nexus/handler.go
+++ go-service/nexus/handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	nexus_microservice "github.com/AdrianWangs/ai-nexus/go-service/nexus/kitex_gen/nexus_microservice"
 )
 
@@ -8,6 +9,39 @@ import (
 type NexusServiceImpl struct{}
 
 func (s *NexusServiceImpl) EchoServer(req *nexus_microservice.AskRequest, stream nexus_microservice.NexusService_EchoServerServer) (err error) {
-	println("EchoServer called")
+
+	fmt.Print("EchoServer")
+
+	fmt.Println(req)
+
+	choices := make([]*nexus_microservice.Choice, 0)
+
+	message := &nexus_microservice.Message{
+		Role:    "System",
+		Content: "你好，我是AI助手，有什么可以帮助你的吗？",
+	}
+
+	choice := &nexus_microservice.Choice{
+		FinishReason: nil,
+		Message:      []*nexus_microservice.Message{message},
+		Index:        0,
+	}
+
+	choices = append(choices, choice)
+
+	askResponse := &nexus_microservice.AskResponse{
+		Id:      "",
+		Model:   "",
+		Choices: choices,
+	}
+
+	err = stream.Send(askResponse)
+
+	defer stream.Close()
+
+	if err != nil {
+		fmt.Println("EchoServer failed: %v", err)
+	}
+
 	return
 }
---
+++ go-service/nexus/test/ask_test.go
@@ -0,0 +1,59 @@
+// @Author Adrian.Wang 2024/8/23 下午5:03:00
+package ask_test
+
+import (
+	"context"
+	"github.com/AdrianWangs/ai-nexus/go-service/nexus/kitex_gen/nexus_microservice"
+	"github.com/AdrianWangs/ai-nexus/go-service/nexus/kitex_gen/nexus_microservice/nexusservice"
+	"github.com/cloudwego/kitex/client/streamclient"
+	"io"
+	"testing"
+)
+
+func TestAsk(t *testing.T) {
+	var streamClient = nexusservice.MustNewStreamClient(
+		"nexus-service", // Service Name
+		streamclient.WithHostPorts("127.0.0.1:8888"), // streamclient.Option...
+	)
+
+	message := &nexus_microservice.Message{
+		Role:    "System",
+		Content: "你好，我是AI助手，有什么可以帮助你的吗？",
+	}
+
+	ctx := context.Background()
+	askRequest := &nexus_microservice.AskRequest{
+		Model:           nil,
+		TopP:            nil,
+		Temperature:     nil,
+		PresencePenalty: nil,
+		MaxTokens:       nil,
+		Seed:            nil,
+		Stop:            nil,
+		EnableSearch:    nil,
+		Messages:        []*nexus_microservice.Message{message},
+	}
+	stream, err := streamClient.EchoServer(ctx, askRequest)
+
+	if err != nil {
+		t.Errorf("EchoServer failed: %v", err)
+		return
+	}
+
+	for {
+		resp, err := stream.Recv()
+
+		if err != nil && err == io.EOF {
+			break
+		}
+
+		if err != nil && err.Error() != "EOF" {
+			t.Errorf("EchoServer failed: %v", err)
+			break
+		}
+
+		t.Log("resp:", resp)
+
+	}
+
+}
