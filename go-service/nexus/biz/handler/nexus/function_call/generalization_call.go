2e801d5aeeb4d9cdb9bb48f1d40c946d4e5d8d3a
Adrian Wang <wyz17601402786@gmail.com>
2024-09-12 19:03:53 +0800

    FEAT: Added a library related to generalization calls

---
+++ go-service/nexus/biz/handler/nexus/function_call/generalization_call.go
@@ -0,0 +1,9 @@
+// Package function_call
+// @Author Adrian.Wang 2024/9/12 18:54:00
+// 泛化调用主要负责对于微服务进行泛化调用
+package function_call
+
+func GeneralizationCall(functionName string, params string) string {
+
+	return ""
+}
